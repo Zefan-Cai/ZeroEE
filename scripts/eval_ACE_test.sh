2d50a0939443b1ed57a61e869dabbe0d190ca4fe
Zefan-Cai <2201210580@stu.pku.edu.cn>
2023-08-13 18:25:44 +0800

    update

--- scripts/eval_ACE_test.sh
+++ scripts/eval_ACE_test.sh
@@ -3,10 +3,10 @@ export CUDA_VISIBLE_DEVICES=0
 # cot
 python -m open_instruct.eval.ace.run_eval \
     --data_dir /local1/zefan/data/ace \
-    --valid_file ACE_valid.json \
+    --valid_file ACE_valid-clean.json \
     --test_file ACE_test.json \
-    --save_dir /local1/zefan/results/ace_ace_bart-large/ \
-    --model /local1/zefan/output/ace_13_bart-large/ \
+    --save_dir /local1/zefan/results/test/ \
+    --model /local1/zefan/output/bart-large-ACE-13/epoch_3/ \
     --tokenizer /local1/zefan/models/bart-large/ \
-    --eval_batch_size 32
+    --eval_batch_size 4
 
