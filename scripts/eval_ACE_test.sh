cb113c9af35746581921192cb7331aa33571f1d9
Zefan-Cai <2201210580@stu.pku.edu.cn>
2023-08-13 18:32:59 +0800

    update

--- scripts/eval_ACE_test.sh
+++ scripts/eval_ACE_test.sh
@@ -6,7 +6,7 @@ python -m open_instruct.eval.ace.run_eval_bart \
     --valid_file ACE_valid-clean.json \
     --test_file ACE_test.json \
     --save_dir /local1/zefan/results/test/ \
-    --model /local1/zefan/output/bart-large-ACE-13/epoch_3/ \
+    --model /local1/zefan/output/bart-large-ACE-13/epoch_5/ \
     --tokenizer /local1/zefan/models/bart-large/ \
     --eval_batch_size 4
 
