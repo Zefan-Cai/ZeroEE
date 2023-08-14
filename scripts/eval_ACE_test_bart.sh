9ace130234ef1907e9d987d0bb4bc5cbdca0e923
Zefan-Cai <2201210580@stu.pku.edu.cn>
2023-08-14 19:21:31 +0800

    update

--- scripts/eval_ACE_test_bart.sh
+++ scripts/eval_ACE_test_bart.sh
@@ -5,8 +5,8 @@ python -m open_instruct.eval.ace.run_eval_bart \
     --data_dir /local1/zefan/data/degree_ed_ace05e_defi \
     --valid_file dev.json \
     --test_file test.json \
-    --save_dir /local1/zefan/results/test_ace_bart_large_degree_epoch8/ \
-    --model /local1/zefan/output/bart-large-degree/epoch_8/ \
+    --save_dir /local1/zefan/results/test_ace_bart_large_degree_epoch7/ \
+    --model /local1/zefan/output/bart-large-degree/epoch_7/ \
     --tokenizer /local1/zefan/models/bart-large/ \
     --eval_batch_size 128
 
