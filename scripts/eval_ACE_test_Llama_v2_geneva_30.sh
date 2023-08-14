fba5605d6a3c2e71ad3adaface8e9ea839f8ca88
Zefan-Cai <2201210580@stu.pku.edu.cn>
2023-08-14 17:44:24 +0800

    update

--- scripts/eval_ACE_test_Llama_v2_geneva_30.sh
+++ scripts/eval_ACE_test_Llama_v2_geneva_30.sh
@@ -1,12 +1,12 @@
 export CUDA_VISIBLE_DEVICES=7
 
 # cot
-python -m open_instruct.eval.ace.run_eval_degree \
-    --data_dir /local1/zefan/data/degree_ed_ace05e_defi \
-    --valid_file dev.json \
-    --test_file test.json \
+python -m open_instruct.eval.ace.run_eval \
+    --data_dir /local1/zefan/data/ace \
+    --valid_file ACE_valid-clean.json \
+    --test_file ACE_valid.json \
     --save_dir /local1/zefan/results//Llama-2-7b-geneva-30-ace-degree/ \
-    --model /local1/zefan/output/Llama-2-7b-geneva-30/epoch_28 \
+    --model /local1/zefan/output/Llama-2-7b-geneva-30/epoch_29 \
     --tokenizer /local1/zefan/models/Llama-2-7b-hf/ \
     --eval_batch_size 8
 
