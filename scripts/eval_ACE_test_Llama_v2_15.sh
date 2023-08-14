cf717c81ec8f3180211e55728370a9e9d09d4550
Zefan-Cai <2201210580@stu.pku.edu.cn>
2023-08-14 17:01:06 +0800

    update

---
+++ scripts/eval_ACE_test_Llama_v2_15.sh
@@ -0,0 +1,12 @@
+export CUDA_VISIBLE_DEVICES=0
+
+# cot
+python -m open_instruct.eval.ace.run_eval \
+    --data_dir /local1/zefan/data/degree_ed_ace05e_defi \
+    --valid_file dev.json \
+    --test_file test.json \
+    --save_dir /local1/zefan/results/test_Llama/ \
+    --model /local1/zefan/output/Llama-2-7b-ace-15/epoch_41/ \
+    --tokenizer /local1/zefan/models/Llama-2-7b-hf/ \
+    --eval_batch_size 4
+
