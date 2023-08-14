e8d30a4c4548fed2519b7a8af38b55097de145bb
Zefan-Cai <2201210580@stu.pku.edu.cn>
2023-08-14 23:38:07 +0800

    update

--- scripts/eval_ACE_test_Llama_v2_15_179.sh
+++ scripts/eval_ACE_test_Llama_v2_15_179.sh
@@ -3,8 +3,8 @@ export CUDA_VISIBLE_DEVICES=7
 # cot
 python -m open_instruct.eval.ace.run_eval \
     --data_dir /local1/zefan/data/ace \
-    --valid_file ACE_valid-clean.json \
-    --test_file ACE_valid.json \
+    --valid_file ACE_valid.json \
+    --test_file ACE_test.json \
     --save_dir /local1/zefan/results/test_Llama-2-7b-geneva-15-179/ \
     --model /local1/zefan/output/Llama-2-7b-geneva-15-179/epoch_7/ \
     --tokenizer /local1/zefan/models/Llama-2-7b-hf/ \
