34a31e6d25b97549e27c4c529e10300477c69528
Zefan-Cai <2201210580@stu.pku.edu.cn>
2023-08-14 18:11:51 +0800

    update

--- scripts/eval_ACE_test_Llama_v2_geneva_30_degree.sh
+++ scripts/eval_ACE_test_Llama_v2_geneva_30_degree.sh
@@ -6,7 +6,7 @@ python -m open_instruct.eval.ace.run_eval_degree \
     --valid_file dev.json \
     --test_file test.json \
     --save_dir /local1/zefan/results//Llama-2-7b-geneva-30-ace-degree/ \
-    --model /local1/zefan/output/Llama-2-7b-geneva-30/epoch_25 \
+    --model /local1/zefan/output/Llama-2-7b-geneva-30/epoch_28 \
     --tokenizer /local1/zefan/models/Llama-2-7b-hf/ \
     --eval_batch_size 8
 
