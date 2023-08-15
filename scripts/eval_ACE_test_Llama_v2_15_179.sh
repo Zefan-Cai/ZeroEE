a16568ec000c8c72abd83eb31f2807e1bd86723b
Zefan-Cai <2201210580@stu.pku.edu.cn>
2023-08-15 13:03:17 +0800

    update

--- scripts/eval_ACE_test_Llama_v2_15_179.sh
+++ scripts/eval_ACE_test_Llama_v2_15_179.sh
@@ -6,7 +6,7 @@ python -m open_instruct.eval.ace.run_eval \
     --valid_file ACE_valid.json \
     --test_file ACE_test.json \
     --save_dir /local1/zefan/results/test_Llama-2-7b-geneva-15-179/ \
-    --model /local1/zefan/output/Llama-2-7b-geneva-15-179/epoch_7/ \
+    --model /local1/zefan/output/Llama-2-7b-geneva-15-179/epoch_42/ \
     --tokenizer /local1/zefan/models/Llama-2-7b-hf/ \
     --eval_batch_size 8
 
