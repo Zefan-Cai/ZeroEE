005d30241e88696930e6af395dfa1bc06d1465e4
Zefan-Cai <2201210580@stu.pku.edu.cn>
2023-09-11 20:27:59 +0800

    update

--- scripts/92_eval_ACE_test_Llama_v2_geneva_20_40_10_epoch20.sh
+++ scripts/92_eval_ACE_test_Llama_v2_geneva_20_40_10_epoch20.sh
@@ -5,8 +5,8 @@ python -m open_instruct.eval.ace.run_eval \
     --data_dir /home/caizf/projects/ZeroEE/data/ace \
     --valid_file ACE_valid.json \
     --test_file ACE_test.json \
-    --save_dir /home/caizf/projects/ZeroEE/results/test-Llama-2-7b-geneva-20-40-10-epoch20/ \
-    --model /home/caizf/projects/ZeroEE/output/Llama-2-7b-geneva-20-40-10/epoch_20/ \
+    --save_dir /home/caizf/projects/ZeroEE/results/test-Llama-2-7b-geneva-20-40-10-epoch18/ \
+    --model /home/caizf/projects/ZeroEE/output/Llama-2-7b-geneva-20-40-10/epoch_18/ \
     --tokenizer /home/models/Llama-2-7b-hf/ \
     --eval_batch_size 8
 
