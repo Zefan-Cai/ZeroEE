7bbc2ce1e870ee83a4181073c50f77514eabd829
Zefan-Cai <2201210580@stu.pku.edu.cn>
2023-09-11 22:58:03 +0800

    update

--- scripts/finetune_with_accelerate_llama_v2_geneva_15_77_5.sh
+++ scripts/finetune_with_accelerate_llama_v2_geneva_15_77_5.sh
@@ -1,8 +1,8 @@
-export CUDA_VISIBLE_DEVICES=4,5,6,7
+export CUDA_VISIBLE_DEVICES=4,5
 
 MODEL_SIZE=7b
-NUM_GPUS=4
-BATCH_SIZE_PER_GPU=8
+NUM_GPUS=2
+BATCH_SIZE_PER_GPU=16
 TOTAL_BATCH_SIZE=32
 GRADIENT_ACC_STEPS=$(($TOTAL_BATCH_SIZE/$NUM_GPUS/$BATCH_SIZE_PER_GPU))
 echo "Training gt model ${MODEL_SIZE} using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size per GPU, $GRADIENT_ACC_STEPS gradient accumulation steps"
@@ -13,8 +13,8 @@ accelerate launch \
     --num_machines 1 \
     --num_processes $NUM_GPUS \
     --use_deepspeed \
-    --deepspeed_config_file ./open_instruct/ds_configs/stage3_no_offloading_accelerate.conf \
-    ./open_instruct/open_instruct/finetune.py \
+    --deepspeed_config_file ./ZeroEE/open_instruct/ds_configs/stage3_no_offloading_accelerate.conf \
+    ./ZeroEE/open_instruct/open_instruct/finetune.py \
     --model_name_or_path /local1/zefan/models/Llama-2-7b-hf/ \
     --use_flash_attn \
     --tokenizer_name /local1/zefan/models/Llama-2-7b-hf/ \
@@ -28,8 +28,8 @@ accelerate launch \
     --lr_scheduler_type linear \
     --warmup_ratio 0.03 \
     --weight_decay 0. \
-    --num_train_epochs 30 \
-    --output_dir /local1/zefan/output/Llama-2-${MODEL_SIZE}-geneva-15-77-5/ \
+    --num_train_epochs 20 \
+    --output_dir /local1/zefan/output/Llama-2-${MODEL_SIZE}-geneva-15-5-77/ \
     --with_tracking \
     --report_to wandb \
     --logging_steps 1 \
