75a23afac102955dc21427ee9a12985b7d58e471
Zefan-Cai <2201210580@stu.pku.edu.cn>
2023-09-11 18:32:42 +0800

    update

---
+++ scripts/92_eval_ACE_test_Llama_v2_geneva_20_40_10_epoch15.sh
@@ -0,0 +1,12 @@
+export CUDA_VISIBLE_DEVICES=7
+
+# cot
+python -m open_instruct.eval.ace.run_eval \
+    --data_dir /home/caizf/projects/ZeroEE/data/ace \
+    --valid_file ACE_valid.json \
+    --test_file ACE_test.json \
+    --save_dir /home/caizf/projects/ZeroEE/results/test-Llama-2-7b-geneva-20-40-10-epoch15/ \
+    --model /home/caizf/projects/ZeroEE/output/Llama-2-7b-geneva-20-40-10/epoch_15/ \
+    --tokenizer /home/models/Llama-2-7b-hf/ \
+    --eval_batch_size 8
+
