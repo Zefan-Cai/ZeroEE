800c15ee3139122fb5f18963ef7673d1fa02a46a
Zefan-Cai <2201210580@stu.pku.edu.cn>
2023-09-12 17:55:53 +0800

    update

--- scripts/92_finetune_with_accelerate_llama_v2_geneva_15_77_5_3.sh
+++ scripts/92_finetune_with_accelerate_llama_v2_geneva_15_77_5_3.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,6
+export CUDA_VISIBLE_DEVICES=2,3,4,5,6,7
 
 MODEL_SIZE=7b
 NUM_GPUS=6
@@ -19,7 +19,7 @@ accelerate launch \
     --use_flash_attn \
     --tokenizer_name /home/models/Llama-2-7b-hf/ \
     --use_slow_tokenizer \
-    --train_file /home/caizf/projects/ZeroEE/data_sample/GENEVA_train_negatives15_samples20_events77.json \
+    --train_file /home/caizf/projects/ZeroEE/data_definition/GENEVA_train_negatives15_samples5_events77_definition3.json \
     --max_seq_length 256 \
     --preprocessing_num_workers 16 \
     --per_device_train_batch_size $BATCH_SIZE_PER_GPU \
@@ -29,7 +29,7 @@ accelerate launch \
     --warmup_ratio 0.03 \
     --weight_decay 0. \
     --num_train_epochs 30 \
-    --output_dir /home/caizf/projects/ZeroEE/output/Llama-2-${MODEL_SIZE}-geneva-15-77-20/ \
+    --output_dir /home/caizf/projects/ZeroEE/output/Llama-2-${MODEL_SIZE}-geneva-15-77-5-3/ \
     --with_tracking \
     --report_to tensorboard \
     --logging_steps 1 \
