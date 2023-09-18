0ffb910bad2e203f5d7b45b9d738c3a5b2318be1
JustBluce <972281745@qq.com>
2023-09-19 01:00:39 +0800

    update

--- scripts/eval_ACE_test_Llama_v2_geneva_20_96_5.sh
+++ scripts/eval_ACE_test_Llama_v2_geneva_20_96_5.sh
@@ -3,7 +3,7 @@ export CUDA_VISIBLE_DEVICES=2
  # @Author: JustBluce 972281745@qq.com
  # @Date: 2023-09-19 00:54:58
  # @LastEditors: JustBluce 972281745@qq.com
- # @LastEditTime: 2023-09-19 00:57:58
+ # @LastEditTime: 2023-09-19 01:00:30
  # @FilePath: /ZeroEE/ZeroEE/scripts/eval_ACE_test_Llama_v2_geneva_15_77_5.sh
  # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 ### 
@@ -14,7 +14,7 @@ python -m open_instruct.eval.ace.run_eval \
     --valid_file ACE_valid.json \
     --test_file ACE_test.json \
     --save_dir /local1/zefan/results/test_Llama-2-7b-geneva-20-96-5-epoch10/ \
-    --model /local1/zefan/output/Llama-2-7b-geneva-15-5-96/epoch_10/ \
+    --model /local1/zefan/output/Llama-2-7b-geneva-20-5-96/epoch_10/ \
     --tokenizer /local1/zefan/models/Llama-2-7b-hf/ \
     --eval_batch_size 8
 
@@ -24,7 +24,7 @@ python -m open_instruct.eval.ace.run_eval \
     --valid_file ACE_valid.json \
     --test_file ACE_test.json \
     --save_dir /local1/zefan/results/test_Llama-2-7b-geneva-20-96-5-epoch15/ \
-    --model /local1/zefan/output/Llama-2-7b-geneva-15-5-96/epoch_15/ \
+    --model /local1/zefan/output/Llama-2-7b-geneva-20-5-96/epoch_15/ \
     --tokenizer /local1/zefan/models/Llama-2-7b-hf/ \
     --eval_batch_size 8
 
@@ -34,7 +34,7 @@ python -m open_instruct.eval.ace.run_eval \
     --valid_file ACE_valid.json \
     --test_file ACE_test.json \
     --save_dir /local1/zefan/results/test_Llama-2-7b-geneva-20-96-5-epoch19/ \
-    --model /local1/zefan/output/Llama-2-7b-geneva-15-5-96/epoch_19/ \
+    --model /local1/zefan/output/Llama-2-7b-geneva-20-5-96/epoch_19/ \
     --tokenizer /local1/zefan/models/Llama-2-7b-hf/ \
     --eval_batch_size 8
 
