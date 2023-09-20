22b85a65b0c132821943c9d4d79a6b2baf910687
Zefan-Cai <2201210580@stu.pku.edu.cn>
2023-09-20 11:14:21 +0800

    update

--- scripts/92_cal_scores.py
+++ scripts/92_cal_scores.py
@@ -147,7 +147,7 @@ def get_trigger(examples):
 
 examples = []
 
-path = '/home/caizf/projects/ZeroEE/results/test-Llama-2-7b-geneva-15-77-40-epoch10/predictions/ACE_valid.jsonl'
+path = '/home/caizf/projects/ZeroEE/results/test-Llama-2-7b-geneva-15-77-5-3-epoch10/predictions/ACE_valid.jsonl'
 
 print(path)
 
