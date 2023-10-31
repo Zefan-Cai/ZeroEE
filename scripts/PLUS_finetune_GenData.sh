b62ba40ad3186d81c4a4b1170d4dd6777560e914
Zefan-Cai <2201210580@stu.pku.edu.cn>
2023-10-31 12:20:12 +0800

    update

--- scripts/PLUS_finetune_GenData.sh
+++ scripts/PLUS_finetune_GenData.sh
@@ -2,8 +2,8 @@ export CUDA_VISIBLE_DEVICES=0,1
 
 MODEL_SIZE=7b
 NUM_GPUS=2
-BATCH_SIZE_PER_GPU=
-TOTAL_BATCH_SIZE=8
+BATCH_SIZE_PER_GPU=8
+TOTAL_BATCH_SIZE=16
 GRADIENT_ACC_STEPS=$(($TOTAL_BATCH_SIZE/$NUM_GPUS/$BATCH_SIZE_PER_GPU))
 echo "Training gt model ${MODEL_SIZE} using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size per GPU, $GRADIENT_ACC_STEPS gradient accumulation steps"
 
@@ -14,7 +14,7 @@ accelerate launch \
     --num_processes $NUM_GPUS \
     --use_deepspeed \
     --deepspeed_config_file ./open_instruct/ds_configs/stage3_no_offloading_accelerate.conf \
-    ./open_instruct/open_instruct/finetune_degree.py \
+    ./open_instruct/open_instruct/finetune_GenData.py \
     --model_name_or_path /local1/zefan/models/Llama-2-7b-hf/ \
     --use_flash_attn \
     --tokenizer_name /local1/zefan/models/Llama-2-7b-hf/ \
@@ -28,10 +28,9 @@ accelerate launch \
     --lr_scheduler_type linear \
     --warmup_ratio 0.03 \
     --weight_decay 0. \
-    --num_train_epochs 45 \
-    --output_dir /local1/zefan/output/Llama-2-${MODEL_SIZE}-ace-degree/ \
+    --num_train_epochs 2 \
+    --output_dir /local1/zefan/output/Llama-2-7b-GenData/ \
     --with_tracking \
     --report_to tensorboard \
-    --logging_steps 1 \
-    --use_lora True
+    --logging_steps 1\
     --checkpointing_steps epoch
