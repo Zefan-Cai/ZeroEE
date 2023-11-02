d2d235574774fe452016f20d1ad8b3b495a5f697
Zefan-Cai <2201210580@stu.pku.edu.cn>
2023-11-02 16:11:01 +0800

    update

--- scripts/PLUS_cal_scores.py
+++ scripts/PLUS_cal_scores.py
@@ -146,7 +146,7 @@ def get_trigger(examples):
  
 base_path = '/local1/zefan/results/Llama-2-7b-GenData-1definitions/'
 
-path = base_path + '/predictions/ACE_valid.jsonl'
+path = base_path + '/predictions/ACE_valid-clean.jsonl'
 
 
 examples = []
