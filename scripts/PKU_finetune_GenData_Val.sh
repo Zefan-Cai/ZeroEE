46e4f11c5e5a4e2335cb1392d0e32cc128c71d5e
Zefan-Cai <2201210580@stu.pku.edu.cn>
2023-11-04 01:01:25 +0800

    update

--- scripts/PKU_finetune_GenData_Val.sh
+++ scripts/PKU_finetune_GenData_Val.sh
@@ -22,7 +22,7 @@ accelerate launch \
     --num_processes $NUM_GPUS \
     --use_deepspeed \
     --deepspeed_config_file ./open_instruct/ds_configs/stage3_no_offloading_accelerate.conf \
-    ./open_instruct/open_instruct/finetune_Val.py \
+    ./open_instruct/open_instruct/finetune_val.py \
     --model_name_or_path $MODEL_PATH \
     --use_flash_attn \
     --tokenizer_name $MODEL_PATH \
