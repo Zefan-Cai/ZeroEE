ea34c05912ae67446d60e1b928a5335b564327c0
Zefan-Cai <2201210580@stu.pku.edu.cn>
2023-11-04 02:36:49 +0800

    update

--- scripts/plus_generated_training_data_1definitions.py
+++ scripts/plus_generated_training_data_1definitions.py
@@ -14,7 +14,24 @@ negative_train_data = []
 
 error_num = 0
 
-for parent_event in data.keys():
+val_parent_list = list(data.keys())[:10]
+train_parent_list = list(data.keys())[10:]
+train_parent_list_100 = list(data.keys())[10:110]
+train_parent_list_200 = list(data.keys())[10:210]
+
+
+
+
+
+
+
+
+
+
+
+
+
+for parent_event in train_parent_list:
     
     sons = data[parent_event]["sons"]
     events = data[parent_event]["events"]
@@ -111,3 +128,342 @@ with open(os.path.join(output_dir, 'generated_data', 'train_1definitions.json'),
 
 
 
+
+
+
+
+
+
+
+
+
+for parent_event in train_parent_list_100:
+    
+    sons = data[parent_event]["sons"]
+    events = data[parent_event]["events"]
+    
+    text_sons = ", ".join(sons)
+    
+
+    for event in events:
+        
+        negative_events = copy.deepcopy(events)
+        negative_events.remove(event)
+        
+        if "name" in data[parent_event]["data"][event].keys():
+            event_name = data[parent_event]["data"][event]["name"]
+            event_definition = data[parent_event]["data"][event]["definition"]
+            triggers = data[parent_event]["data"][event]["triggers"]
+            samples = data[parent_event]["data"][event]["samples"]
+
+            diverse_definitions = copy.deepcopy(data[parent_event]["data"][event]["rewrite_definitions"])
+            diverse_definitions = diverse_definitions[:4]
+            diverse_definitions.append(event_definition)
+            
+            
+            # for definition in diverse_definitions:
+            for sample in samples:
+                
+                sentence = sample["sentence"]
+                trigger = sample["trigger"]
+                
+                selected_trigger = random.choice(triggers)
+            
+                positive_train_data.append({
+                    # "Event definition": definition,
+                    # "Event type": event,
+                    # "Event name": event_name,    
+                    # "Event triggers": triggers,
+                    # "trigger": trigger,
+                    # "selected_trigger": selected_trigger,
+                    # "sentence": sentence,
+                    # "parent": parent_event,
+                    # "events": events,
+                    # "sons": sons,
+                    "prompt": f"SENTENCE: {sentence} \n EVENT TYPE: {event_name}. \n DEFINITION: {event_definition} \n PARENT: {parent_event}, SON: {text_sons}. \n So what is the trigger?",
+                    "completion": f"Event trigger is {trigger}."
+                    })
+
+            for negative_event in negative_events:
+                
+                if "name" in data[parent_event]["data"][negative_event].keys():
+                    negative_event_name = data[parent_event]["data"][negative_event]["name"]
+                    negative_event_definition = data[parent_event]["data"][negative_event]["definition"]
+                    negative_triggers = data[parent_event]["data"][negative_event]["triggers"]
+                    negative_samples = data[parent_event]["data"][negative_event]["samples"]
+                    
+                    for negative_sample in negative_samples:
+                        
+                        negative_sentence = negative_sample["sentence"]
+                        negative_trigger = negative_sample["trigger"]
+                        
+                        negative_selected_trigger = random.choice(negative_triggers)
+                    
+                        negative_train_data.append({
+                            # "Event definition": definition,
+                            # "Event type": event,
+                            # "Event name": event_name,    
+                            # "Event triggers": triggers,
+                            # "trigger": "<trigger>",
+                            # "selected_trigger": selected_trigger,
+                            # "sentence": negative_sentence,
+                            # "parent": parent_event,
+                            # "events": events,
+                            # "sons": sons,
+                            "prompt": f"SENTENCE: {sentence} \n EVENT TYPE: {event_name}. \n DEFINITION: {event_definition} \n PARENT: {parent_event}, SON: {text_sons}. \n So what is the trigger?",
+                            "completion": f"Event trigger is <trigger>."
+                            })
+        else:
+            error_num += 1
+
+print(f"debug len positive_train_data {str(len(positive_train_data))}")
+print(f"debug len negative_train_data {str(len(negative_train_data))}")
+print(f"debug error_num {str(error_num)}")
+
+train_data =  positive_train_data + negative_train_data
+
+with open(os.path.join(output_dir, 'generated_data', 'train_1definitions_100.json'), 'w') as fp:
+    for line in tqdm(train_data):
+        json.dump(line, fp)
+        fp.write('\n')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+for parent_event in train_parent_list_200:
+    
+    sons = data[parent_event]["sons"]
+    events = data[parent_event]["events"]
+    
+    text_sons = ", ".join(sons)
+    
+
+    for event in events:
+        
+        negative_events = copy.deepcopy(events)
+        negative_events.remove(event)
+        
+        if "name" in data[parent_event]["data"][event].keys():
+            event_name = data[parent_event]["data"][event]["name"]
+            event_definition = data[parent_event]["data"][event]["definition"]
+            triggers = data[parent_event]["data"][event]["triggers"]
+            samples = data[parent_event]["data"][event]["samples"]
+
+            diverse_definitions = copy.deepcopy(data[parent_event]["data"][event]["rewrite_definitions"])
+            diverse_definitions = diverse_definitions[:4]
+            diverse_definitions.append(event_definition)
+            
+            
+            # for definition in diverse_definitions:
+            for sample in samples:
+                
+                sentence = sample["sentence"]
+                trigger = sample["trigger"]
+                
+                selected_trigger = random.choice(triggers)
+            
+                positive_train_data.append({
+                    # "Event definition": definition,
+                    # "Event type": event,
+                    # "Event name": event_name,    
+                    # "Event triggers": triggers,
+                    # "trigger": trigger,
+                    # "selected_trigger": selected_trigger,
+                    # "sentence": sentence,
+                    # "parent": parent_event,
+                    # "events": events,
+                    # "sons": sons,
+                    "prompt": f"SENTENCE: {sentence} \n EVENT TYPE: {event_name}. \n DEFINITION: {event_definition} \n PARENT: {parent_event}, SON: {text_sons}. \n So what is the trigger?",
+                    "completion": f"Event trigger is {trigger}."
+                    })
+
+            for negative_event in negative_events:
+                
+                if "name" in data[parent_event]["data"][negative_event].keys():
+                    negative_event_name = data[parent_event]["data"][negative_event]["name"]
+                    negative_event_definition = data[parent_event]["data"][negative_event]["definition"]
+                    negative_triggers = data[parent_event]["data"][negative_event]["triggers"]
+                    negative_samples = data[parent_event]["data"][negative_event]["samples"]
+                    
+                    for negative_sample in negative_samples:
+                        
+                        negative_sentence = negative_sample["sentence"]
+                        negative_trigger = negative_sample["trigger"]
+                        
+                        negative_selected_trigger = random.choice(negative_triggers)
+                    
+                        negative_train_data.append({
+                            # "Event definition": definition,
+                            # "Event type": event,
+                            # "Event name": event_name,    
+                            # "Event triggers": triggers,
+                            # "trigger": "<trigger>",
+                            # "selected_trigger": selected_trigger,
+                            # "sentence": negative_sentence,
+                            # "parent": parent_event,
+                            # "events": events,
+                            # "sons": sons,
+                            "prompt": f"SENTENCE: {sentence} \n EVENT TYPE: {event_name}. \n DEFINITION: {event_definition} \n PARENT: {parent_event}, SON: {text_sons}. \n So what is the trigger?",
+                            "completion": f"Event trigger is <trigger>."
+                            })
+        else:
+            error_num += 1
+
+print(f"debug len positive_train_data {str(len(positive_train_data))}")
+print(f"debug len negative_train_data {str(len(negative_train_data))}")
+print(f"debug error_num {str(error_num)}")
+
+train_data =  positive_train_data + negative_train_data
+
+with open(os.path.join(output_dir, 'generated_data', 'train_1definitions_200.json'), 'w') as fp:
+    for line in tqdm(train_data):
+        json.dump(line, fp)
+        fp.write('\n')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+positive_val_data = []
+negative_val_data = []
+
+
+
+for parent_event in val_parent_list:
+    
+    sons = data[parent_event]["sons"]
+    events = data[parent_event]["events"]
+    
+    text_sons = ", ".join(sons)
+    
+
+    for event in events:
+        
+        negative_events = copy.deepcopy(events)
+        negative_events.remove(event)
+        
+        if "name" in data[parent_event]["data"][event].keys():
+            event_name = data[parent_event]["data"][event]["name"]
+            event_definition = data[parent_event]["data"][event]["definition"]
+            triggers = data[parent_event]["data"][event]["triggers"]
+            samples = data[parent_event]["data"][event]["samples"]
+
+            diverse_definitions = copy.deepcopy(data[parent_event]["data"][event]["rewrite_definitions"])
+            diverse_definitions = diverse_definitions[:4]
+            diverse_definitions.append(event_definition)
+            
+            
+            # for definition in diverse_definitions:
+            for sample in samples:
+                
+                sentence = sample["sentence"]
+                trigger = sample["trigger"]
+                
+                selected_trigger = random.choice(triggers)
+            
+                positive_val_data.append({
+                    # "Event definition": definition,
+                    # "Event type": event,
+                    # "Event name": event_name,    
+                    # "Event triggers": triggers,
+                    # "trigger": trigger,
+                    # "selected_trigger": selected_trigger,
+                    # "sentence": sentence,
+                    # "parent": parent_event,
+                    # "events": events,
+                    # "sons": sons,
+                    "prompt": f"SENTENCE: {sentence} \n EVENT TYPE: {event_name}. \n DEFINITION: {event_definition} \n PARENT: {parent_event}, SON: {text_sons}. \n So what is the trigger?",
+                    "completion": f"Event trigger is ",
+                    "trigger": f"{trigger}"
+                    })
+
+            for negative_event in negative_events:
+                
+                if "name" in data[parent_event]["data"][negative_event].keys():
+                    negative_event_name = data[parent_event]["data"][negative_event]["name"]
+                    negative_event_definition = data[parent_event]["data"][negative_event]["definition"]
+                    negative_triggers = data[parent_event]["data"][negative_event]["triggers"]
+                    negative_samples = data[parent_event]["data"][negative_event]["samples"]
+                    
+                    for negative_sample in negative_samples:
+                        
+                        negative_sentence = negative_sample["sentence"]
+                        negative_trigger = negative_sample["trigger"]
+                        
+                        negative_selected_trigger = random.choice(negative_triggers)
+                    
+                        negative_val_data.append({
+                            # "Event definition": definition,
+                            # "Event type": event,
+                            # "Event name": event_name,    
+                            # "Event triggers": triggers,
+                            # "trigger": "<trigger>",
+                            # "selected_trigger": selected_trigger,
+                            # "sentence": negative_sentence,
+                            # "parent": parent_event,
+                            # "events": events,
+                            # "sons": sons,
+                            "prompt": f"SENTENCE: {sentence} \n EVENT TYPE: {event_name}. \n DEFINITION: {event_definition} \n PARENT: {parent_event}, SON: {text_sons}. \n So what is the trigger?",
+                            "completion": f"Event trigger is ",
+                            "trigger":  "<trigger>"
+                            })
+        else:
+            error_num += 1
+
+print(f"debug len positive_train_data {str(len(positive_val_data))}")
+print(f"debug len negative_train_data {str(len(negative_val_data))}")
+print(f"debug error_num {str(error_num)}")
+
+val_data =  positive_val_data + negative_val_data
+
+with open(os.path.join(output_dir, 'generated_data', 'val_1definitions.json'), 'w') as fp:
+    for line in tqdm(val_data):
+        json.dump(line, fp)
+        fp.write('\n')
