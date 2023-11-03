886c15a60ed11bc8bc4d3ad9099d6d72c7a06ed5
Zefan-Cai <2201210580@stu.pku.edu.cn>
2023-11-03 23:48:08 +0800

    update

--- scripts/PLUS_eval_ACE_GenData_5definitions_ACE_v1.sh
+++ scripts/PLUS_eval_ACE_GenData_5definitions_ACE_v1.sh
@@ -5,8 +5,8 @@ python -m open_instruct.eval.ace.run_eval \
     --data_dir /local1/zefan/data/ace_v1 \
     --valid_file ACE_valid_GenerationStyle.json \
     --test_file ACE_test_GenerationStyle.json \
-    --save_dir /local1/zefan/results/Llama-2-7b-GenData-5definitions/ \
-    --model /local1/zefan/output/Llama-2-7b-GenData-5definitions/ \
+    --save_dir /local1/zefan/results/Llama-2-7b-GenData-1definitions_ace_v1/ \
+    --model /local1/zefan/output/Llama-2-7b-GenData-1definitions/ \
     --tokenizer /local1/zefan/models/Llama-2-7b-hf/ \
     --eval_batch_size 8
 
