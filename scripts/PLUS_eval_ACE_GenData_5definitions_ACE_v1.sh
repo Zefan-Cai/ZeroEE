fc60970362ad286b946318840e248f814bede89c
Zefan-Cai <2201210580@stu.pku.edu.cn>
2023-11-03 23:38:39 +0800

    update

--- scripts/PLUS_eval_ACE_GenData_5definitions_ACE_v1.sh
+++ scripts/PLUS_eval_ACE_GenData_5definitions_ACE_v1.sh
@@ -3,8 +3,8 @@ export CUDA_VISIBLE_DEVICES=7
 # cot
 python -m open_instruct.eval.ace.run_eval \
     --data_dir /local1/zefan/data/ace_v1 \
-    --valid_file ACE_valid-clean.json \
-    --test_file ACE_valid.json \
+    --valid_file ACE_valid_GenerationStyle_clean.json \
+    --test_file ACE_valid_GenerationStyle.json \
     --save_dir /local1/zefan/results/Llama-2-7b-GenData-5definitions/ \
     --model /local1/zefan/output/Llama-2-7b-GenData-5definitions/ \
     --tokenizer /local1/zefan/models/Llama-2-7b-hf/ \
