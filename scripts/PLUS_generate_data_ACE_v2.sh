652521e314dbb82213a41fc068efeb66413532d8
JustBluce <972281745@qq.com>
2024-01-01 14:21:29 +0800

    update

--- scripts/PLUS_generate_data_ACE_v2.sh
+++ scripts/PLUS_generate_data_ACE_v2.sh
@@ -3,12 +3,12 @@
  # @Author: JustBluce 972281745@qq.com
  # @Date: 2023-11-22 20:48:25
  # @LastEditors: JustBluce 972281745@qq.com
- # @LastEditTime: 2023-11-23 15:51:20
+ # @LastEditTime: 2024-01-01 14:18:56
  # @FilePath: /ZeroEE/ZeroEE/scripts/PLUS_generate_data_ACE_v2.sh
  # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 ### 
 service=plus
-template=v1
+template=v2
 
 base_dir="/local1/zefan/"
 
