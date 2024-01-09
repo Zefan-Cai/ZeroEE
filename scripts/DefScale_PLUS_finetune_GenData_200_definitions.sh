de494a4f61f86f10d08f47741e91c85d0832d99a
Zefan-Cai <2201210580@stu.pku.edu.cn>
2024-01-08 22:27:45 -0800

    add definition scaling

---
+++ scripts/DefScale_PLUS_finetune_GenData_200_definitions.sh
@@ -0,0 +1,61 @@
+export CUDA_VISIBLE_DEVICES="$1"
+export NumDef="$2"
+export NumEvents="200"
+###
+ # @Author: JustBluce 972281745@qq.com
+ # @Date: 2023-11-22 18:59:18
+ # @LastEditors: JustBluce 972281745@qq.com
+ # @LastEditTime: 2023-12-23 12:20:58
+ # @FilePath: /ZeroEE/ZeroEE/scripts/PLUS_finetune_GenData_200_5definitions.sh
+ # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+### 
+
+MODEL_PATH="/local1/zefan/models/Llama-2-7b-hf/"
+
+OUTPUT_NAME=DefScaleNewGenDatav2_200_${NumDef}definition/
+TRAIN_FILE=/local1/zefan/data/generated_data/train_200_${NumDef}definitions_v2.json
+VAL_FILE="/local1/zefan/data/generated_data/valid_100_1definitions_v2.json"
+TEST_FILE="/local1/zefan/data/ace_v2/ACE_test_v2_trigger.json"
+METRICS_FILE="/local1/zefan/ZeroEE/open_instruct/compute_score_ee.py"
+REPORT_TAGS="ZeroEE"
+
+ceildiv(){ echo $((($1+$2-1)/$2)); }
+NUM_GPUS=$(ceildiv ${#CUDA_VISIBLE_DEVICES} 2)
+BATCH_SIZE_PER_GPU=4
+TOTAL_BATCH_SIZE=96
+GRADIENT_ACC_STEPS=$(($TOTAL_BATCH_SIZE/$NUM_GPUS/$BATCH_SIZE_PER_GPU))
+echo "Training model ${MODEL_PATH} using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size per GPU, $GRADIENT_ACC_STEPS gradient accumulation steps"
+
+accelerate launch \
+    --main_process_port 22453 \
+    --mixed_precision bf16 \
+    --num_machines 1 \
+    --num_processes $NUM_GPUS \
+    --use_deepspeed \
+    --deepspeed_config_file /local1/zefan/ZeroEE/open_instruct/ds_configs/stage3_no_offloading_accelerate.conf \
+    /local1/zefan/ZeroEE/open_instruct/open_instruct/finetune_val.py \
+    --model_name_or_path $MODEL_PATH \
+    --use_flash_attn \
+    --tokenizer_name $MODEL_PATH \
+    --use_slow_tokenizer \
+    --train_file $TRAIN_FILE \
+    --val_file $VAL_FILE \
+    --test_file $TEST_FILE \
+    --metrics_file ${METRICS_FILE} \
+    --max_seq_length 256 \
+    --preprocessing_num_workers 16 \
+    --per_device_train_batch_size $BATCH_SIZE_PER_GPU \
+    --per_device_eval_batch_size 32 \
+    --gradient_accumulation_steps $GRADIENT_ACC_STEPS \
+    --learning_rate 2e-5 \
+    --lr_scheduler_type linear \
+    --warmup_ratio 0.03 \
+    --weight_decay 0. \
+    --num_train_epochs 10 \
+    --output_dir output/${OUTPUT_NAME} \
+    --with_tracking \
+    --report_to wandb \
+    --report_name $OUTPUT_NAME \
+    --report_tags $REPORT_TAGS \
+    --checkpointing_steps epoch \
+    --logging_steps 1
---
+++ scripts/PLUS_generate_GenData_v4_events_200_def.sh
@@ -0,0 +1,69 @@
+###
+ # @Author: JustBluce 972281745@qq.com
+ # @Date: 2023-12-23 12:13:54
+ # @LastEditors: JustBluce 972281745@qq.com
+ # @LastEditTime: 2023-12-23 12:17:47
+ # @FilePath: /ZeroEE/ZeroEE/scripts/PLUS_generate_GenData_v3_events200_def5.sh
+ # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+###
+
+# The updated scripts, will use the specific amount of events
+service=plus
+template=v2
+
+
+num_definitions=$1
+event_num=200
+
+base_dir="/local1/zefan/"
+generate_data_dir='ZeroEE/data/generated_data_fix_v5.json'
+output_dir='data/generated_data'
+
+output_train_filename=train_${event_num}_${num_definitions}definitions_v2.json
+output_valid_filename='valid_100_1definitions_v2.json'
+
+num_negative_sample=10
+num_negative_inOntology=3
+
+
+train_parent_start=50
+train_parent_end=1000000
+valid_parent_start=0
+valid_parent_end=50
+
+random_seed=42
+
+python ./generate_train_data.py \
+    --service ${service} \
+    --template_version ${template} \
+    --base_dir ${base_dir} \
+    --generate_data_dir ${generate_data_dir} \
+    --output_dir ${output_dir} \
+    --output_train_filename ${output_train_filename} \
+    --output_valid_filename ${output_valid_filename} \
+    --num_negative_sample ${num_negative_sample} \
+    --num_negative_inOntology ${num_negative_inOntology} \
+    --num_definitions ${num_definitions} \
+    --random_seed ${random_seed} \
+    --train_parent_start ${train_parent_start} \
+    --train_parent_end ${train_parent_end} \
+    --valid_parent_start ${valid_parent_start} \
+    --valid_parent_end ${valid_parent_end} \
+    --train_events $event_num
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
