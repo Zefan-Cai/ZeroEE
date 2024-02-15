42b5047f2c6c12f2af4ed4f7ec42a3cdd11256a5
billkunghappy <billkunghappy.happy@gmail.com>
2024-02-15 02:14:13 -0800

    update

---
+++ scripts_v2/PLUS_generate_data_rawACE_v2_DegreeDefi.sh
@@ -0,0 +1,55 @@
+
+###
+ # @Author: JustBluce 972281745@qq.com
+ # @Date: 2023-11-22 20:48:25
+ # @LastEditors: JustBluce 972281745@qq.com
+ # @LastEditTime: 2024-01-01 14:18:56
+ # @FilePath: /ZeroEE/ZeroEE/scripts/PLUS_generate_data_ACE_v2.sh
+ # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+### 
+service=plus
+template=v2
+
+base_dir="/local1/zefan/"
+
+# base_dir='/home/caizf/projects/ZeroEE/'
+
+data_info_dir='ZeroEE/data/'
+output_dir='data/ace_v2'
+definition_file='ACE_event_definition_DEGREE.json'
+ontology_file='ACE_ontology_v0.json'
+
+# output_valid_filename='ACE_valid_v2_trigger.json'
+# output_test_filename='ACE_test_v2_trigger.json'
+
+# If add setting inference, use this name
+output_valid_filename='ACE_valid_v2_Degree_inference.json'
+output_test_filename='ACE_test_v2_Degree_inference.json'
+
+python ./generate_rawACE_data.py \
+    --service ${service} \
+    --template_version ${template} \
+    --base_dir ${base_dir} \
+    --data_info_dir ${data_info_dir} \
+    --output_dir ${output_dir} \
+    --output_valid_filename ${output_valid_filename} \
+    --output_test_filename ${output_test_filename} \
+    --definition_file ${definition_file} \
+    --ontology_file ${ontology_file} \
+    --setting inference
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ scripts_v2/generate_rawACE_data.py
@@ -0,0 +1,263 @@
+import os
+import json
+import argparse
+from tqdm import tqdm
+
+
+
+
+class Data():
+    def __init__(self, args):
+        self.args = args
+        
+        
+        with open(os.path.join(self.args.base_dir, self.args.data_info_dir, self.args.ontology_file), 'r') as fp:
+            self.ACE_ontology = json.load(fp)
+
+        ## ACE Event Definition
+        with open(os.path.join(self.args.base_dir, self.args.data_info_dir, self.args.definition_file), 'r') as fp:
+            self.event_type2definition = json.load(fp)
+        self.event_list = list(self.event_type2definition.keys())
+
+        self.get_valid_data()
+        self.get_test_data()
+
+    def get_valid_data(self):
+
+        ## ACE val data
+        ACE_valid_data = []
+
+        with open(os.path.join(self.args.base_dir, 'ZeroEE', 'oneie_ace05_en_event', 'val.json'), 'r') as fp:
+            for line in fp.readlines():
+                ACE_valid_data.append(json.loads(line))
+                
+        valid_data = []
+
+        for index in range(len(ACE_valid_data)):
+            
+            event_type2trigger = {}
+            sample_data_list = []
+            
+            for event_index in range(len(ACE_valid_data[index]["event"])):
+                event_type = ACE_valid_data[index]["event"][event_index]["type"]
+                trigger = ACE_valid_data[index]["event"][event_index]["text"]
+                
+                
+                if event_type not in event_type2trigger.keys():
+                    event_type2trigger[event_type] = []
+                event_type2trigger[event_type].append(trigger)
+
+            for event_type in self.event_type2definition.keys():
+                event_definition = self.event_type2definition[event_type]
+                
+                for parent_event in self.ACE_ontology.keys():
+                    if event_type == parent_event: break
+                    if event_type in self.ACE_ontology[parent_event]:break
+                
+                sons = self.ACE_ontology[parent_event]
+                text_sons = ", ".join(sons)
+                
+                if event_type in event_type2trigger.keys():
+                    trigger = event_type2trigger[event_type]
+                    if self.args.setting == "inference":
+                        pass
+                    elif self.args.setting == "evaluation":
+                        trigger = " and ".join(trigger)
+                else:
+                    trigger = "<trigger>"
+                
+                sample = ACE_valid_data[index]["text"]
+                
+                event_type_id = self.event_list.index(event_type)
+                    
+                if self.args.template_version == "v1":
+                    prompt = f"{sample} \n The event is: {event_type}. \n The event definition is: {event_definition} \n The parent event is {parent_event}, son events include {text_sons}. \n So what is the trigger? Event trigger is "
+                    if self.args.setting == "inference":
+                        prompt = f"{sample} \n The event is: {event_type}. \n The event definition is: {event_definition} \n The parent event is {parent_event}, son events include {text_sons}. \n So what is the trigger? Event trigger is "
+                elif self.args.template_version == "v2":
+                    prompt = f"SENTENCE: {sample} \n EVENT TYPE: {event_type}. \n DEFINITION: {event_definition} \n PARENT: {parent_event}, SON: {text_sons}. \n So what is the trigger? Event trigger is "
+                    if self.args.setting == "inference":
+                        prompt = f"SENTENCE: {sample} \n EVENT TYPE: {event_type}. \n DEFINITION: {event_definition} \n PARENT: {parent_event}, SON: {text_sons}. \n So what is the trigger? Event trigger is "
+                elif self.args.template_version == "v3":
+                    prompt = f"{sample} \n {event_type} event \n {event_definition} \n So what is the trigger? Event trigger is "
+                    if self.args.setting == "inference":
+                        prompt = f"{sample} \n {event_type} event \n {event_definition} \n So what is the trigger? Event trigger is "
+                else:
+                    raise Exception("THe template version should be v1 or v2")
+
+                if self.args.setting == "evaluation":
+                    sample_data_list.append({
+                        "data_id": index,
+                        "event_type": event_type_id,
+                        "prompt": prompt,
+                        "completion": f"Event trigger is ",
+                        "trigger": trigger
+                        })
+                elif self.args.setting == "inference":
+                    sample_data_list.append({
+                        "data_id": index,
+                        "event_type": event_type_id,
+                        "prompt": prompt,
+                        "completion": f"Event trigger is ",
+                        "trigger": trigger,
+                        "Event definition": event_definition,
+                        "Event type": event_type
+                        })
+
+            valid_data.append(sample_data_list)
+            
+
+        with open(os.path.join(self.args.base_dir, self.args.output_dir, self.args.output_valid_filename), 'w') as fp:
+            if self.args.setting == "evaluation":
+                for index_i in range(len(valid_data)):
+                    for index_j in range(len(valid_data[index_i])):
+                        json.dump(valid_data[index_i][index_j], fp)
+                        fp.write('\n')
+            elif self.args.setting == "inference":
+                for index_i in range(len(valid_data)):
+                    
+                    
+                    json.dump(valid_data[index_i], fp)
+                    fp.write('\n')
+
+    def get_test_data(self):
+
+        ## ACE test data
+
+        ACE_test_data = []
+
+        with open(os.path.join(self.args.base_dir, 'ZeroEE', 'oneie_ace05_en_event', 'test.json'), 'r') as fp:
+            for line in fp.readlines():
+                ACE_test_data.append(json.loads(line))
+
+
+        test_data = []
+
+        for index in range(len(ACE_test_data)):
+
+            event_type2trigger = {}
+            sample_data_list = []
+
+            if ACE_test_data[index]["event"] == []: pass
+            else:
+                for event_index in range(len(ACE_test_data[index]["event"])):
+                    event_type = ACE_test_data[index]["event"][event_index]["type"]
+                    trigger = ACE_test_data[index]["event"][event_index]["text"]
+
+                    if event_type not in event_type2trigger.keys():
+                        event_type2trigger[event_type] = []
+                    event_type2trigger[event_type].append(trigger)
+
+
+
+            for event_type in self.event_type2definition.keys():
+                event_definition = self.event_type2definition[event_type]
+                
+                for parent_event in self.ACE_ontology.keys():
+                    if event_type == parent_event: break
+                    if event_type in self.ACE_ontology[parent_event]:break
+                
+                sons = self.ACE_ontology[parent_event]
+                text_sons = ", ".join(sons)
+                
+                if event_type in event_type2trigger.keys():
+                    trigger = event_type2trigger[event_type]
+                    if self.args.setting == "inference":
+                        pass
+                    elif self.args.setting == "evaluation":
+                        trigger = " and ".join(trigger)
+                else:
+                    trigger = "<trigger>"
+
+
+
+                sample = ACE_test_data[index]["text"]
+                
+                event_type_id = self.event_list.index(event_type)
+                
+                if self.args.template_version == "v1":
+                    prompt = f"{sample} \n The event is: {event_type}. \n The event definition is: {event_definition} \n The parent event is {parent_event}, son events include {text_sons}. \n So what is the trigger? Event trigger is "
+                    if self.args.setting == "inference":
+                        prompt = f"{sample} \n The event is: {event_type}. \n The event definition is: {event_definition} \n The parent event is {parent_event}, son events include {text_sons}. \n So what is the trigger? Event trigger is "
+                elif self.args.template_version == "v2":
+                    prompt = f"SENTENCE: {sample} \n EVENT TYPE: {event_type}. \n DEFINITION: {event_definition} \n PARENT: {parent_event}, SON: {text_sons}. \n So what is the trigger? Event trigger is "
+                    if self.args.setting == "inference":
+                        prompt = f"SENTENCE: {sample} \n EVENT TYPE: {event_type}. \n DEFINITION: {event_definition} \n PARENT: {parent_event}, SON: {text_sons}. \n So what is the trigger? Event trigger is "
+                elif self.args.template_version == "v3":
+                    prompt = f"{sample} \n {event_type} event \n {event_definition} \n So what is the trigger? Event trigger is "
+                    if self.args.setting == "inference":
+                        prompt = f"{sample} \n {event_type} event \n {event_definition} \n So what is the trigger? Event trigger is "
+                else:
+                    raise Exception("THe template version should be v1 or v2")
+
+                if self.args.setting == "evaluation":
+                    sample_data_list.append({
+                        "data_id": index,
+                        "event_type": event_type_id,
+                        "prompt": prompt,
+                        "completion": f"Event trigger is ",
+                        "trigger": trigger
+                        })
+                elif self.args.setting == "inference":
+                    sample_data_list.append({
+                        "data_id": index,
+                        "event_type": event_type_id,
+                        "prompt": prompt,
+                        "completion": f"Event trigger is ",
+                        "trigger": trigger,
+                        "Event definition": event_definition,
+                        "Event type": event_type
+                        })
+            test_data.append(sample_data_list)
+
+        with open(os.path.join(self.args.base_dir, self.args.output_dir, self.args.output_test_filename), 'w') as fp:
+            if self.args.setting=="evaluation":
+                for index_i in range(len(test_data)):
+                    for index_j in range(len(test_data[index_i])):
+                        json.dump(test_data[index_i][index_j], fp)
+                        fp.write('\n')
+            elif self.args.setting=="inference":
+                for index_i in range(len(test_data)):
+                    json.dump(test_data[index_i], fp)
+                    fp.write('\n')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    parser = argparse.ArgumentParser(description="")
+
+    parser.add_argument('--service', default='plus', type=str, help='plus or pku')
+    parser.add_argument('--template_version', default='v2', type=str, help='v1 or v2')
+    parser.add_argument('--base_dir', default='/local1/zefan/', type=str, help=' plus: /local1/zefan/   pku: /home/caizf/projects/ZeroEE/ ')
+    parser.add_argument('--data_info_dir', default='/ZeroEE/data/', type=str, help='dir to generated data')
+    parser.add_argument('--output_dir', default='/data/ace_v2', type=str, help='dir to generated data')
+    parser.add_argument('--output_test_filename', default='ACE_test_v2_trigger.json', type=str, help='train filename')
+    parser.add_argument('--output_valid_filename', default='ACE_valid_v2_trigger.json', type=str, help='valid filename')
+    parser.add_argument('--definition_file', default='ACE_event_definition.json', type=str, help='definition filename')
+    parser.add_argument('--ontology_file', default='ACE_ontology.json', type=str, help='ontology filename')
+
+    parser.add_argument('--setting', default='evaluation', type=str, help='evaluation or inference')
+
+    args = parser.parse_args()
+
+    data = Data(args)
+
+
+
+
+if __name__ == "__main__":
+    main()
